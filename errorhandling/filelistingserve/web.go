ced02072625e949381dcd161a1160d55bbe5399f
Jormin <jorminxee@gmail.com>
2020-10-11 21:01:13 +0800

    :sparkles: feat(7.5): 服务器统一出错处理2
    Signed-off-by: Jormin <jorminxee@gmail.com>

--- errorhandling/filelistingserve/web.go
+++ errorhandling/filelistingserve/web.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"go-study/errorhandling/filelistingserve/filelisting"
 	"go-study/modules/log"
 	"net/http"
@@ -11,9 +12,20 @@ type appHandle func(writer http.ResponseWriter, request *http.Request) error
 
 func errWrapper(handle appHandle) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			r := recover()
+			if r != nil {
+				log.Info(fmt.Sprintf("Panic: %v", r))
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+		}()
 		err := handle(w, r)
 		if err != nil {
 			log.Warn("Error Handling Request: %s\n", err.Error())
+			if userErr, ok := err.(UserError); ok {
+				http.Error(w, userErr.Message(), http.StatusBadRequest)
+				return
+			}
 			code := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
@@ -28,8 +40,13 @@ func errWrapper(handle appHandle) func(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type UserError interface {
+	error
+	Message() string
+}
+
 func main() {
-	http.HandleFunc("/list/", errWrapper(filelisting.HandleFileList))
+	http.HandleFunc("/", errWrapper(filelisting.HandleFileList))
 	err := http.ListenAndServe(":8090", nil)
 	if err != nil {
 		panic(err)
